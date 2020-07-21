5149be596b37b766b5592635fa6b9e7d755f3b8c
Tyler Mayoff <tyler@tylermayoff.com>
2020-07-21 17:56:08 -0400

    Added everything

---
+++ Assets/Scripts/Boid.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.TextCore.LowLevel;
+
+public class Boid : MonoBehaviour {
+
+    public float viewRadius;
+    public float viewAngle;
+    public float speed;
+    public float turnSpeed;
+
+    private SpriteRenderer sprite;
+    private BoidManager boidManager;
+
+    public List<Transform> neighbors;
+
+    private Vector3 desiredVelocity;
+    private Vector3 desiredAngle;
+
+    private void Awake() {
+        boidManager = GetComponentInParent<BoidManager>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    void Start() {
+        neighbors = new List<Transform>();
+
+        Color[] colors = new Color[] {
+            new Color32(255, 173, 173, 255),
+            new Color32(255, 214, 165, 255),
+            new Color32(253, 255, 182, 255),
+            new Color32(202, 255, 191, 255),
+            new Color32(155, 246, 255, 255),
+            new Color32(160, 196, 255, 255),
+            new Color32(189, 178, 255, 255),
+            new Color32(255, 198, 255, 255)
+        };
+        sprite.color = colors[Random.Range(0, colors.Length)];
+    }
+
+    void Update() {
+
+        if (boidManager.DebugLines) {
+            foreach (Transform b in neighbors) {
+                Popcron.Gizmos.Line(transform.position, b.position, Color.green);
+            }
+
+            Popcron.Gizmos.Line(transform.position, transform.position + transform.up, Color.blue);
+
+            if (desiredVelocity != Vector3.zero) {
+                Popcron.Gizmos.Line(transform.position, transform.position + desiredVelocity.normalized, Color.red);
+            }
+        }
+
+        // Check bounds
+        Vector3 boundVelocity = Vector3.zero;
+        if (!boidManager.Bound.Contains(transform.position)) {
+            // Above Bound
+            if (transform.position.y > boidManager.Bound.max.y - 1) {
+                boundVelocity += new Vector3(0, -(transform.position.y + boidManager.Bound.max.y));
+            }
+
+            //Below bound
+            if (transform.position.y < boidManager.Bound.min.y + 1) {
+                boundVelocity += new Vector3(0, -(transform.position.y + boidManager.Bound.min.y));
+            }
+
+            // Left
+            if (transform.position.x < boidManager.Bound.min.x + 1) {
+                boundVelocity += new Vector3(-(transform.position.x + boidManager.Bound.min.x), 0);
+            }
+
+            // Right
+            if (transform.position.x > boidManager.Bound.max.x - 1) {
+                boundVelocity += new Vector3(-(transform.position.x + boidManager.Bound.max.x), 0);
+            }
+        }
+
+        Vector3 centerVelocity = Vector3.zero;
+        Vector3 collideVelocity = Vector3.zero;
+        Vector3 matchVelocity = Vector3.zero;
+
+        Vector3 mouseVelocity =  boidManager.mousePosition - transform.position;
+        if (mouseVelocity.sqrMagnitude > viewRadius * viewRadius)
+            mouseVelocity = Vector3.zero;
+        mouseVelocity.Normalize();
+
+        if (neighbors.Count > 0) {
+            foreach (Transform n in neighbors) {
+
+                // Cohesion
+                if (boidManager.Cohesion)
+                    centerVelocity += n.position;
+
+                // Seperation
+                if (boidManager.Seperation)
+                    if (Vector3.Distance(transform.position, n.position) < 1f)
+                        collideVelocity -= n.position - transform.position;
+
+                // Alignment
+                if (boidManager.Alignment)
+                    matchVelocity += n.up;
+            }
+            if (boidManager.Cohesion) {
+                centerVelocity /= neighbors.Count;
+                centerVelocity -= transform.position;
+            }
+            if (boidManager.Alignment)
+                matchVelocity /= neighbors.Count;
+        }
+
+        desiredVelocity = boundVelocity;
+        if (boidManager.Alignment)
+            desiredVelocity += matchVelocity;
+        if (boidManager.Seperation)
+            desiredVelocity += collideVelocity;
+        if (boidManager.Cohesion)
+            desiredVelocity += centerVelocity;
+        if (boidManager.MouseAttract)
+            desiredVelocity += mouseVelocity;
+        if (boidManager.MouseRepel)
+            desiredVelocity -= mouseVelocity;
+
+        if (desiredVelocity == Vector3.zero) desiredVelocity = transform.up;
+
+        desiredAngle = new Vector3(0, 0, Vector3.SignedAngle(Vector3.up, desiredVelocity, Vector3.forward));
+
+        // Rotate the boid
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(desiredAngle), turnSpeed * Time.deltaTime);
+
+        // Move the boid
+        transform.position += transform.up * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/BoidManager.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking.Types;
+using UnityEngine.UI;
+
+public class BoidManager : MonoBehaviour {
+
+    public Text populationText;
+    public Toggle mouseAttractToggle;
+    public Toggle mouseRepelToggle;
+
+    public GameObject BoidPrefab;
+
+    public int population;
+
+    public int populationPool = 100;
+
+    public bool MouseAttract = false;
+    public bool MouseRepel = false;
+    public bool Alignment = true;
+    public bool Cohesion = true;
+    public bool Seperation = true;
+    public bool DebugLines = false;
+
+    public Bounds Bound;
+
+    public List<Transform> Boids;
+
+    public Vector3 mousePosition;
+
+    private int oldPopulation;
+
+    private void Start() {
+        populationText.text = "Population: " + population;
+
+        for (int i = 0; i < populationPool; i++) {
+            Vector3 position = new Vector3(Random.Range(Bound.min.x, Bound.max.x), Random.Range(Bound.min.y, Bound.max.y));
+            GameObject b = Instantiate(BoidPrefab, position, Quaternion.identity, transform);
+            if (i >= population)
+                b.SetActive(false);
+            Boids.Add(b.transform);
+        }
+    }
+
+    private void Update() {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+
+        if (DebugLines) {
+            Popcron.Gizmos.Cube(Bound.center, Quaternion.identity, Bound.size, Color.green);
+        }
+
+        foreach (Transform t in Boids) {
+            Boid b = t.GetComponent<Boid>();
+
+            // Get neighbors
+            foreach (Transform n in Boids) {
+                // Skip current boid
+                if (n == t) continue;
+
+                // Remove and skip boids outside of view
+                Vector3 heading = n.position - t.position;
+                float angle = Mathf.Atan2(heading.y, heading.x) * Mathf.Rad2Deg - 90;
+                if (heading.sqrMagnitude > b.viewRadius * b.viewRadius || angle > b.viewAngle || angle < -b.viewAngle || !n.gameObject.activeSelf || !n.gameObject.activeInHierarchy) {
+                    b.neighbors.Remove(n);
+                    continue;
+                }
+
+                // Add if not already there
+                if (!b.neighbors.Contains(n))
+                    b.neighbors.Add(n);
+            }
+        }
+
+        if (population != oldPopulation) {
+            if (population > populationPool) {
+                AddToPool();
+            }
+
+            for (int i = 0; i < populationPool; i++) {
+                if (i >= population) Boids[i].gameObject.SetActive(false);
+                else Boids[i].gameObject.SetActive(true);
+            }
+        }
+
+        oldPopulation = population;
+    }
+
+    public void UpdatePopulation(float newPop) {
+        population = (int)newPop;
+        populationText.text = "Population: " + population;
+    }
+
+    public void UpdateSeperation(bool value) {
+        Seperation = value;
+    }
+
+    public void UpdateCohesion(bool value) {
+        Cohesion = value;
+    }
+
+    public void UpdateAlignment(bool value) {
+        Alignment = value;
+    }
+
+    public void UpdateDebug(bool value) {
+        DebugLines = value;
+    }
+
+    public void UpdateMouseAttract (bool value) {
+        MouseAttract = value;
+        if (value) {
+            mouseRepelToggle.isOn = false;
+            MouseRepel = false;
+        }
+    } 
+
+    public void UpdateMouseRepel(bool value) {
+        MouseRepel = value;
+        if (value) {
+            mouseAttractToggle.isOn = false;
+            MouseAttract = false;
+        }
+    }
+
+
+    private void AddToPool() {
+        for (int i = 0; i < 50; i++) {
+            Vector3 position = new Vector3(Random.Range(Bound.min.x, Bound.max.x), Random.Range(Bound.min.y, Bound.max.y));
+            GameObject b = Instantiate(BoidPrefab, position, Quaternion.identity, transform);
+            if (i >= population)
+                b.SetActive(false);
+            Boids.Add(b.transform);
+        }
+
+        populationPool += 50;
+    }
+
+    private void OnDrawGizmos() {
+    }
+}
